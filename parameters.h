86bef5c8ad8fdfd1cc7270d3802b0f57a163f79f
Logrus <idlogrus@gmail.com>
2016-07-06 00:47:56 +0200

    Fix parameters order.

--- parameters.h
+++ parameters.h
@@ -23,19 +23,19 @@ struct Parameters{
 void inline read_parameters(int argc, char *argv[], Parameters &p){
  using namespace std;
  if (argc==1){
-   cout<<argv[0]<<" input_file[tiff,avi] [out_filename] [sim_th] [hard_th]"<<endl;
+   cout<<argv[0]<<" input_file[tiff,avi] [sim_th] [hard_th]"<<endl;
    exit(0);
  }
 
  if (argc >= 2) p.filename = argv[1];
- if (argc >= 3) p.filename = argv[2];
- if (argc >= 4) p.sim_th = atof(argv[3]);
- if (argc >= 5) p.hard_th = atof(argv[4]);
+ //if (argc >= 3) p.filename = argv[2];
+ if (argc >= 3) p.sim_th = atof(argv[2]);
+ if (argc >= 4) p.hard_th = atof(argv[3]);
 
- if (argc >= 6) p.window_size = atoi(argv[5]);
- if (argc >= 7) p.step_size = atoi(argv[6]);
+ if (argc >= 5) p.window_size = atoi(argv[4]);
+ if (argc >= 6) p.step_size = atoi(argv[5]);
  
- if (argc >= 8) p.gpu_device = atoi(argv[7]);
+ if (argc >= 7) p.gpu_device = atoi(argv[6]);
  
 
  cout<<"Parameters:"<<endl;
