8f77cdf39a33f3b52e7396f8acc54559ebea2426
Logrus <idlogrus@gmail.com>
2016-07-05 23:05:21 +0200

    Cleanup and organise parameters.

--- parameters.h
+++ parameters.h
@@ -4,41 +4,46 @@
 #include <iomanip>
 
 struct Parameters{
-  std::string filename = "data2/t.txt";
-  int   patch_size=4;   // Patch size
-  int   window_size=5; // Search window (size=window_size)
-  int   step_size=3;
-  float sim_th=2500.0;   // Similarity threshold for the first step
-  int   maxN=16;          // Maximal number of the patches in one group
-  float hard_th=2.7; // Hard schrinkage threshold
-  float sigma=25.0;
-  float noise_sigma=25.0;
-  bool  add_noise_ = false;
-  int   gpu_device=0;
+  // Modifyable
+  std::string filename = "gflowersg15/t.txt";
+  std::string out_filename = "";
+  float sim_th = 2500.0;   // Similarity threshold for the first step
+  float hard_th = 2.7; // Hard schrinkage threshold
+
+  // Can be changed but not advisable
+  int   window_size = 5; // Search window (size=window_size)
+  int   step_size = 3;
+  int   gpu_device = -1;
+
+  // Fixed in current implementation
+  int   patch_size=4;   // Patch size, changing this can lead to trouble
+  int   maxN=16;        // Maximal number of the patches in one group
 };
 
 void inline read_parameters(int argc, char *argv[], Parameters &p){
  using namespace std;
  if (argc==1){
-   cout<<argv[0]<<" avi patch_size window_size sim_th maxN hard_th sigma"<<endl;
+   cout<<argv[0]<<" input_file[tiff,avi] [out_filename] [sim_th] [hard_th]"<<endl;
+   exit(0);
  }
 
- if(argc>=2) p.filename=argv[1];
- if(argc>=3) p.patch_size=atoi(argv[2]);
- if(argc>=4) p.window_size=atoi(argv[3]);
- if(argc>=5) p.sim_th=atof(argv[4]);
- if(argc>=6) p.maxN=atoi(argv[5]);
- if(argc>=7) p.hard_th=atof(argv[6]);
- if(argc>=8){ p.sigma=atof(argv[7]); }
- if(argc>=9){ p.noise_sigma=atof(argv[8]); }
+ if (argc >= 2) p.filename = argv[1];
+ if (argc >= 3) p.filename = argv[2];
+ if (argc >= 4) p.sim_th = atof(argv[3]);
+ if (argc >= 5) p.hard_th = atof(argv[4]);
+
+ if (argc >= 6) p.window_size = atoi(argv[5]);
+ if (argc >= 7) p.step_size = atoi(argv[6]);
+ 
+ if (argc >= 8) p.gpu_device = atoi(argv[7]);
+ 
 
  cout<<"Parameters:"<<endl;
- cout<<"         volume: "<<p.filename.c_str()<<endl;
- cout<<"     patch_size: "<<p.patch_size<<endl;
- cout<<"    window_size: "<<p.window_size<<endl;
- cout<<"         sim_th: "<<fixed<<setprecision(3)<<p.sim_th<<endl;
- cout<<"           maxN: "<<p.maxN<<endl;
- cout<<"        hard_th: "<<fixed<<setprecision(3)<<p.hard_th<<endl;
- cout<<"          sigma: "<<fixed<<setprecision(3)<<p.sigma<<endl;
- cout<<"    noise_sigma: "<<fixed<<setprecision(3)<<p.noise_sigma<<endl;
+ cout<<"             input file: "<<p.filename.c_str()<<endl;
+ cout << " similarity threshold: " << fixed << setprecision(3) << p.sim_th << endl;
+ cout << "       hard threshold: " << fixed << setprecision(3) << p.hard_th << endl;
+ cout << "          window size: " << p.window_size << endl;
+ cout << "            step size: " << p.step_size << endl;
+ cout << " max cubes in a group: " << p.maxN << endl;
+ cout << "           patch size: " << p.patch_size << endl;
 }
