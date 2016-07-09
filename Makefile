a068ef9c4ddef5faffecd346e01d66af5c75f268
Vladislav Tananaev <tananaev@raphael.informatik.uni-freiburg.de>
2016-07-09 18:33:16 +0200

    3.7 on linux on cluster

--- Makefile
+++ Makefile
@@ -6,4 +6,4 @@ all: Makefile main.cpp
 	nvcc -g -G -lineinfo -std=c++11 -I. -I${CUDA_INCLUDE} ${SOURCES} -o main -L/usr/X11R6/lib -lm  -lpthread -lX11  `pkg-config --libs opencv` `pkg-config --cflags opencv`
 
 optim: Makefile main.cpp
-	nvcc -g -G -lineinfo -std=c++11 -I. -I${CUDA_INCLUDE} ${SOURCES} -o main -L/usr/X11R6/lib -lm  -lpthread -lX11 -O2
+	nvcc -std=c++11 -I. -I${CUDA_INCLUDE} ${SOURCES} -o main -L/usr/X11R6/lib -lm  -lpthread -lX11 -O3
--- main.cpp
+++ main.cpp
@@ -1,4 +1,3 @@
-#define cimg_use_tiff
 #pragma comment( lib, "libtiff.lib" )
 #pragma comment( lib, "libtiff-bcc.lib" )
 #include <string>
@@ -41,9 +40,9 @@ int main(int argc, char *argv[]){
   reader.read(p.filename, noisy_image, width, height, depth);
   loading_file.stop(); std::cout<<"Loading file took: "<<loading_file.getSeconds()<<std::endl;
   std::cout << "Volume size: (" << width << ", " << height << ", " << depth<< ") total: " << width*height*depth << std::endl;
-  CImg<unsigned char> test2(noisy_image.data(), width, height, depth, 1); test2.display();
-  test2.save_tiff("noisy.tiff");
-  //reader.read("gt/t.txt", gt, width, height, depth);
+  //CImg<unsigned char> test2(noisy_image.data(), width, height, depth, 1); test2.display();
+  //test2.save_tiff("noisy.tiff");
+  reader.read("gt/t.txt", gt, width, height, depth);
 
 
   // Run first step of BM4D
@@ -53,8 +52,8 @@ int main(int argc, char *argv[]){
   bm4d_timing.stop(); std::cout<<"BM4D total time: "<<bm4d_timing.getSeconds()<<std::endl;
   
   // Save image
-  CImg<unsigned char> test(denoised_image.data(), width, height, depth, 1, 1); test.display();
-  test.save_tiff("denoised.tiff");
+  //CImg<unsigned char> test(denoised_image.data(), width, height, depth, 1, 1); test.display();
+  //test.save_tiff("denoised.tiff");
   std::cout << "PSNR noisy: " << psnr(gt, noisy_image) << std::endl;
   std::cout << "PSNR denoised: " << psnr(gt, denoised_image) << std::endl;
   std::cout << "PSNR reconstructed: " << psnr(noisy_image, denoised_image) << std::endl;
