78f2b9368a1d37018580529c8b9047ab4a42ac92
Vladislav Tananaev <v.d.tananaev@gmail.com>
2018-07-25 19:28:49 +0200

    Create a new utils file

--- include/bm4d-gpu/utils.h
+++ include/bm4d-gpu/utils.h
@@ -1,4 +1,25 @@
-#ifndef UTILS_H
-#define UTILS_H
+#pragma once
+#include <vector>
 
-#endif // UTILS_H
+namespace bm4d_gpu {
+
+template <typename T>
+constexpr T sqr(const T& val) { return val*val; }
+
+
+float psnr(const std::vector<unsigned char>& gt, const std::vector<unsigned char>& noisy)
+{
+  const float max_signal{255.f};
+  const float sqr_err{0.f};
+  for (int i = 0; i<gt.size(); ++i)
+  {
+    float diff = gt[i] - noisy[i];
+    sqr_err += diff*diff;
+  }
+  float mse = sqr_err / gt.size();
+  float psnr = 10.f*log10(max_signal*max_signal / mse);
+  return psnr;
+}
+
+} // bm4d_gpu namespace
+
--- src/main.cpp
+++ src/main.cpp
@@ -1,32 +1,21 @@
 /*
-* 2016, Vladislav Tananaev
-* v.d.tananaev [at] gmail [dot] com
-*/
-#include <string>
-#include <iostream>
+ * 2016, Vladislav Tananaev
+ * v.d.tananaev [at] gmail [dot] com
+ */
 #include <bm4d-gpu/allreader.h>
 #include <bm4d-gpu/bm4d.h>
 #include <bm4d-gpu/parameters.h>
+#include <bm4d-gpu/utils.h>
 #include <bm4d-gpu/stopwatch.hpp>
-#include <bm4d-gpu/CImg.h>
-using namespace cimg_library;
 
-float psnr(std::vector<unsigned char>& gt, std::vector<unsigned char>& noisy)
-{
-  float max_signal = 255;
-  float sqr_err = 0;
-  for (int i = 0; i<gt.size(); ++i)
-  {
-    float diff = gt[i] - noisy[i];
-    sqr_err += diff*diff;
-  }
-  float mse = sqr_err / gt.size();
-  float psnr = 10.f*log10(max_signal*max_signal / mse);
-  return psnr;
+#include <iostream>
+#include <string>
+
+namespace {
+namespace bg = bm4d_gpu;
 }
 
-int main(int argc, char *argv[]){
-
+int main(int argc, char* argv[]) {
   // Take parameters
   Parameters p;
   read_parameters(argc, argv, p);
@@ -37,25 +26,25 @@ int main(int argc, char *argv[]){
   int width, height, depth;
 
   // Load volume
-  AllReader reader(false); // true - show debug video on load
-  Stopwatch loading_file(true); // true - start right away
+  AllReader reader(false);       // true - show debug video on load
+  Stopwatch loading_file(true);  // true - start right away
   reader.read(p.filename, noisy_image, width, height, depth);
-  loading_file.stop(); std::cout << "Loading file took: " << loading_file.getSeconds() << std::endl;
-  std::cout << "Volume size: (" << width << ", " << height << ", " << depth << ") total: " << width*height*depth << std::endl;
-  CImg<unsigned char> test2(noisy_image.data(), width, height, depth, 1); test2.display();
+  loading_file.stop();
+  std::cout << "Loading file took: " << loading_file.getSeconds() << std::endl;
+  std::cout << "Volume size: (" << width << ", " << height << ", " << depth
+            << ") total: " << width * height * depth << std::endl;
   reader.read("gt/t.txt", gt, width, height, depth);
 
   // Run first step of BM4D
-  Stopwatch bm4d_timing(true); // true - start right away
+  Stopwatch bm4d_timing(true);  // true - start right away
   BM4D filter(p, noisy_image, width, height, depth);
   std::vector<unsigned char> denoised_image = filter.run_first_step();
-  bm4d_timing.stop(); std::cout<<"BM4D total time: "<<bm4d_timing.getSeconds()<<std::endl;
-  
-  // Save image
-  CImg<unsigned char> test(denoised_image.data(), width, height, depth, 1, 1); test.display();
-  std::cout << "PSNR noisy: " << psnr(gt, noisy_image) << std::endl;
-  std::cout << "PSNR denoised: " << psnr(gt, denoised_image) << std::endl;
-  std::cout << "PSNR reconstructed: " << psnr(noisy_image, denoised_image) << std::endl;
+  bm4d_timing.stop();
+  std::cout << "BM4D total time: " << bm4d_timing.getSeconds() << std::endl;
+
+  std::cout << "PSNR noisy: " << bg::psnr(gt, noisy_image) << std::endl;
+  std::cout << "PSNR denoised: " << bg::psnr(gt, denoised_image) << std::endl;
+  std::cout << "PSNR reconstructed: " << bg::psnr(noisy_image, denoised_image) << std::endl;
 
   return EXIT_SUCCESS;
 }
