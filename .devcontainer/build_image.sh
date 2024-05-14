89dd8d7cac3de7d33fce172c872314c54e24bc00
Vladislav Tananaev <v.d.tananaev@gmail.com>
2024-05-15 00:09:19 +0200

    Update docker

--- .devcontainer/build_image.sh
+++ .devcontainer/build_image.sh
@@ -2,6 +2,25 @@
 
 SCRIPT_DIR=`dirname "$0"`
 
-${SCRIPT_DIR}/dockerfile_from_template.sh
+# The base image and host system should have the same CUDA version
+# This script tries to extract the CUDA version from the nvcc --version output
+# and then replaces the placeholder in the Dockerfile.template with the actual version.
 
-docker build -f $SCRIPT_DIR/Dockerfile . -t bm4d-gpu-devenv
+# If it fails to fuilfil its purpose,
+# you can always manually replace the placeholder with the actual version.
+
+nvcc_output=$(nvcc --version 2>&1)
+
+# Extract the release version using grep and sed
+CUDA_VERSION=$(echo "$nvcc_output" | grep -oP 'release \K\d+\.\d+(\.\d+)?')
+
+if [[ $(echo $CUDA_VERSION | awk -F'.' '{print NF}') -eq 2  ]]; then
+    CUDA_VERSION="${CUDA_VERSION}.0"
+fi
+
+
+docker build --build-arg BASE_IMAGE=nvidia/cuda:${CUDA_VERSION}-devel-ubuntu20.04 \
+             --build-arg USER_NAME=${USER} \
+             --build-arg GROUP_NAME=${USER} \
+             -t bm4d-gpu-devenv \
+             -f $SCRIPT_DIR/Dockerfile . 
